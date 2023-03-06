29bc7918001d8f2d043c01af4abbb995d1d1053b
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-03-07 06:59:03 +0900

    first commit

---
+++ src/photos/seeFeed/seeFeed.resolvers.js
@@ -0,0 +1,34 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeFeed: protectedResolver((_, { offset }, { loggedInUser }) =>
+      client.photo.findMany({
+        take: 2,
+        skip: offset,
+        /*
+        where: {
+          OR: [
+            {
+              user: {
+                followers: {
+                  some: {
+                    id: loggedInUser.id,
+                  },
+                },
+              },
+            },
+            {
+              userId: loggedInUser.id,
+            },
+          ],
+        },
+        */
+        orderBy: {
+          createdAt: "desc",
+        },
+      })
+    ),
+  },
+};
