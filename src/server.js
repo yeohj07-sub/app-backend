419b13142a450d453cd731da07bdf67eddfc2a23
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-03-19 12:51:32 +0900

    2303191251

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
-
+//
 const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   resolvers,
